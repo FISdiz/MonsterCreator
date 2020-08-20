41210eba1756d8d9e04cbadbcdcd9f57c1465746
Felipe Salazar Diaz <felipe.salazar.di@gmail.com>
2020-08-20 15:10:24 -0400

    Initial commit

---
+++ app/src/main/java/cl/desafiolatam/monstercreator/model/db/MonsterDao.kt
@@ -0,0 +1,24 @@
+package cl.desafiolatam.monstercreator.model.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import cl.desafiolatam.monstercreator.model.Monster
+
+/**
+ * Created by Cristian Vidal on 2019-09-26.
+ */
+@Dao
+interface MonsterDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMonster(monster: Monster)
+
+    @Query("SELECT * FROM monster_table ORDER BY name ASC")
+    suspend fun getAllMonsters(): LiveData<List<Monster>>
+
+
+    @Delete
+    suspend fun deleteAllMonsters(vararg monster: Monster)
+
+
+}
